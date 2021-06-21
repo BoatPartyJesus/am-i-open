cb19cdd4d3a78e5e7c66f806736ea74d61c5b9b7
Ash Thompson <ashley.thompson@bglgroup.co.uk>
2021-06-21 09:22:52 +0100

    Ash - working but could probably use a refactor; almost ceratinly not idiomatic

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import * as Koa from "koa";
 import * as Router from "koa-router"
-import { isSameDay, isSaturday, isSunday, addDays, isAfter, isBefore, format, addHours, addMinutes, startOfDay } from "date-fns";
+import { } from "date-fns";
 import {I18n} from "i18n";
 import {join as pathJoin} from "path";
-import getBankHolidays from "./getBankHolidays";
-import * as times from "./times.json"
+import isOpen from "./openingTimes"
 
 const app: Koa = new Koa();
 const router: Router = new Router();
@@ -14,77 +13,16 @@ i18n.configure({
   directory: pathJoin(__dirname, "/locales")
 });
 
-type DateResponse = {
-  isOpen: boolean,
-  message: string
-}
-
 router.get("/open", async (ctx: Koa.Context) => {
-  const bankHolidays = await getBankHolidays();
-  const queryDate = ctx.query.date as string;
+  let dateInQuestion = ctx.query.date ?? Date.now()
+  const queryDate = dateInQuestion as string;
   const date = new Date(queryDate);
-  const tomorrow = addDays(date, 1);
 
-  const isClosed = times.closed.find(closedDate => isSameDay(new Date(closedDate), date));
-  if(isClosed) {
-    ctx.body = "CLOSED";
-    return;
-  }
+  let response = await isOpen(date);
 
-  const isBankholiday = bankHolidays.find(holiday => isSameDay(new Date(holiday.date), date));
-  if(isBankholiday) {
-    ctx.body = JSON.stringify(times.bankHoliday);
-    return;
-  }
-
-  if(isSaturday(date)) {
-    ctx.body = JSON.stringify(times.weekend);
-    return;
-  }
-
-  if(isSunday(date)) {
-    ctx.body = "CLOSED"
-    return;
-  }
-
-  const openingDateTime = calcuateTimeOfDay(date, times.weekday.openingTime);
-  const closingDateTime = calcuateTimeOfDay(date, times.weekday.closingTime);
-
-  if(isBefore(date, openingDateTime)) {
-    const response: DateResponse = {
-      isOpen: false,
-      message: i18n.__("open", format(openingDateTime, "h:mmaaa"), format(closingDateTime, "h:mmaaa"), "today")
-    }
-    ctx.body = JSON.stringify(response);
-    return;
-  }
-
-  if(isAfter(date, closingDateTime)) {
-    const openingDateTime = calcuateTimeOfDay(tomorrow, times.weekday.openingTime);
-    const closingDateTime = calcuateTimeOfDay(tomorrow, times.weekday.closingTime);
-    const response: DateResponse = {
-      isOpen: false,
-      message: i18n.__("open", format(openingDateTime, "h:mmaaa"), format(closingDateTime, "h:mmaaa"), "tomorrow")
-    }
-    ctx.body = JSON.stringify(response);
-    return;
-  }
-  
-  ctx.body = i18n.__("openUntil", times.weekday.closingTime)
+  ctx.body = JSON.stringify(response);
+  return
 });
-
-function calcuateTimeOfDay(date: Date, dateObject: string) {
-  const split = dateObject.split(":");
-  const hours = parseInt(split[0])
-  const minutes = parseInt(split[1]);
-
-  return addHours(addMinutes(startOfDay(date), minutes), hours)
-}
-
-router.get("/", async (ctx: Koa.Context) => {
-  ctx.body = "Samuel L Jackson is Chuck Norris' padawan"
-});
-
 
 app.use(router.routes());
 app.on('error', console.error);
---
+++ src/openingTimes.ts
@@ -0,0 +1,180 @@
+import * as times from "./times.json"
+import {addDays,isBefore, format, isSameDay, isSaturday, isSunday, addHours, addMinutes, startOfDay, isWeekend, isMonday, isThursday, isTuesday, isWednesday, isFriday } from "date-fns";
+import getBankHolidays from "./getBankHolidays";
+
+const dayOfWeek = {MONDAY:"Monday", TUESDAY:"Tuesday", WEDNESDAY:"Wednesday", THURSDAY:"Thursday", FRIDAY:"Friday", SATURDAY:"Saturday", SUNDAY:"Sunday"}
+Object.freeze(dayOfWeek)
+
+const dayType = {WEEKDAY:"weekday", SATURDAY:"saturday", SUNDAY:"sunday", BANKHOLIDAY:"bankHoliday", CLOSED:"closed"}
+Object.freeze(dayType)
+
+type dayTypeDetail = {
+    name: string,
+    type: string,
+    open: boolean,
+    openingTime: Date,
+    closingTime: Date
+}
+
+type DateResponse = {
+    isOpen: boolean,
+    message: string
+  }
+
+async function isOpen(date:Date) {
+
+    let response: DateResponse = {
+      isOpen: false,
+      message: ""
+    }
+  
+    let daysForward = 0;
+    let openOnDay = false;
+  
+    do {
+      var testDate = addDays(date, daysForward);
+  
+      let openToday = await isOpenOn(startOfDay(testDate));
+  
+      if (openToday.open){
+        let nameOfDay = 
+          daysForward === 0 ? "today" :
+          daysForward === 1 ? "tomorrow" :
+          openToday.name;
+  
+        if(daysForward > 0 || isBefore(testDate, openToday.openingTime)) {
+          openOnDay = true;
+          response.message = i18n.__("open", format(openToday.openingTime, "h:mmaaa"), format(openToday.closingTime, "h:mmaaa"), nameOfDay)      
+          break;
+        } else if (isBefore(testDate, openToday.closingTime)){
+          openOnDay = true;
+          response.isOpen = true;
+          response.message = i18n.__("openUntil", format(openToday.closingTime, "h:mmaaa"))
+          break;
+        }
+        else {
+          daysForward++
+        }
+      }
+      else {
+        daysForward++
+      }  
+    }
+    while (!openOnDay)
+  
+    return response
+  }
+
+async function isOpenOn(date: Date){
+    let dateType = await getTypeOfDay(date);
+
+    let response: dayTypeDetail = {
+        name: getNameOfDay(date),
+        type: dateType,
+        open: false,
+        openingTime: null,
+        closingTime: null
+    }
+
+    switch(dateType){
+        case dayType.CLOSED:
+        case dayType.SUNDAY:
+        break
+        case dayType.BANKHOLIDAY:
+        case dayType.SATURDAY:
+        case dayType.WEEKDAY:
+        response.open = true;
+
+        let {open, close} = getOpeningTimes(dateType)
+        response.openingTime = calcuateTimeOfDay(date, open)
+        response.closingTime = calcuateTimeOfDay(date, close)
+        default:
+        break
+    }
+    
+    return response
+}
+
+async function getTypeOfDay(date: Date){
+    const bankHolidays = await getBankHolidays();
+  
+    const isClosed = times.closed.find(closedDate => isSameDay(new Date(closedDate), date));
+    if(isClosed) {
+      return dayType.CLOSED;
+    }
+  
+    const isBankholiday = bankHolidays.find(holiday => isSameDay(new Date(holiday.date), date));
+    if(isBankholiday) {
+      return dayType.BANKHOLIDAY;
+    }
+  
+    if (isSunday(date)){
+      return dayType.SUNDAY
+    }
+  
+    if (isSaturday(date)){
+      return dayType.SATURDAY
+    }
+  
+    if(!isWeekend(date)) {
+      return dayType.WEEKDAY
+    }
+}
+
+function calcuateTimeOfDay(date: Date, dateObject: string) {
+    const split = dateObject.split(":");
+    const hours = parseInt(split[0])
+    const minutes = parseInt(split[1]);
+  
+    return addHours(addMinutes(startOfDay(date), minutes), hours)
+}
+
+function getNameOfDay(date: Date){
+    if(isMonday(date)){
+        return dayOfWeek.MONDAY
+    } else
+    if(isTuesday(date)){
+        return dayOfWeek.TUESDAY
+    } else
+    if(isWednesday(date)){
+        return dayOfWeek.WEDNESDAY
+    } else
+    if(isThursday(date)){
+        return dayOfWeek.THURSDAY
+    } else
+    if(isFriday(date)){
+        return dayOfWeek.FRIDAY
+    } else
+    if(isSaturday(date)){
+        return dayOfWeek.SATURDAY
+    } else
+    if(isSunday(date)){
+        return dayOfWeek.SUNDAY
+    }
+    // seriously... fuck this method
+}
+
+function getOpeningTimes(day: string){
+    let open:string, close:string
+
+    switch(day){
+        case dayType.BANKHOLIDAY:
+        open = times.bankHoliday.openingTime
+        close = times.bankHoliday.closingTime
+        case dayType.SUNDAY:
+        open = times.sunday.openingTime
+        close = times.sunday.closingTime
+        case dayType.SATURDAY:
+        open = times.saturday.openingTime
+        close = times.saturday.closingTime
+        case dayType.WEEKDAY:
+        open = times.weekday.openingTime
+        close = times.weekday.closingTime
+        default:
+        break
+    }
+
+    return {open: open, close: close}
+}
+
+export default isOpen;
